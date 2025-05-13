8f7981173803533712291b8cd87027b7ea47c300
Knightler <aryan.haghighinew@gmail.com>
2025-05-14 00:55:48 +0300

    created the freestanding 'bare-metal' operating system

---
+++ target/debug/blog_os.d
@@ -0,0 +1,1 @@
+C:\Users\Asus\Desktop\rust-os-practice\blog_os\target\debug\blog_os.exe: C:\Users\Asus\Desktop\rust-os-practice\blog_os\src\main.rs
---
+++ target/debug/deps/blog_os.d
@@ -0,0 +1,5 @@
+C:\Users\Asus\Desktop\rust-os-practice\blog_os\target\debug\deps\blog_os.exe: src\main.rs
+
+C:\Users\Asus\Desktop\rust-os-practice\blog_os\target\debug\deps\blog_os.d: src\main.rs
+
+src\main.rs:
